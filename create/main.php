c9fecb487c4affc149698ec79cb391ec6097f99a
unknown <Kotaro Ishiguro@FS.(none)>
2011-10-28 01:11:31 -0700

    Imported changes by Tiphanie.

---
+++ create/main.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Bored No More</title></head>
+<body>
+<h1>Create Event</h1>
+<form>
+Event Name:  <input type="text" name="name"/><br/>
+Event Creator: <input type="text" name="creator"/><br/>
+Event Description: <input type="text" name="description"/><br/>
+Event Location: <input type="text" name="location"/><br/>
+Event Time: <input type="text" name="time"/><br/>
+
+
+
+<input type="submit" value="Create Event"/>
+</form>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>Bored no More</title></head>
+<!--
+So, this is really shoddily done 'cause I do not remember how to make buttons
+-->
+<body>
+
+<h1>Bored No More</h1>
+<h3>Let's Do Something!</h3>
+<ul>
+<li><a href="create/main.php">Create an Event</a></li>
+<li><a href="mine/main.php">My Events</a></li>
+<li><a href="events/main.php">View Events</a></li>
+</ul>
+</body>
+
+</html>
