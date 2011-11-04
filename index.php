77a55f322a3921900d318ff91a36a12430151c5f
unknown <Kotaro Ishiguro@FS.(none)>
2011-11-03 21:52:27 -0700

    changed index.php to integrate with facebook

--- index.php
+++ index.php
@@ -1,20 +1,50 @@
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US"
      xmlns:fb="https://www.facebook.com/2008/fbml"> 
-<head><title>Bored no More</title>
-<head/> 
+<head>
+  <title>Bored no More</title>
+  <meta name="viewport" 
+        content="initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
+</head>
 <body> 
 <div id="fb-root"></div>
-<script src="http://connect.facebook.net/en_US/all.js"></script>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=240585475995480";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<script>
+(function() {
+    var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        document.getElementById('fb-root').appendChild(e);
+        }());
+window.fbAsyncInit = function() {
+    FB.init({ appId: '240585475995480', 
+      status: true, 
+      cookie: true,
+      xfbml: true,
+      oauth: true});
+
+      FB.Event.subscribe('auth.statusChange', handleStatusChange);  
+    };
+function handleStatusChange(response) {
+   document.body.className = response.authResponse ? 'connected' : 'not_connected';
+
+   if (response.authResponse) {
+     console.log(response);
+   }
+ }
+</script>
 
 <h1>Bored No More</h1>
 <h3>Let's Do Something!</h3>
+<div id="login">
+  <p><button onClick="loginUser();">Login</button></p>
+</div>
+<div id="logout">
+  <p><button  onClick="FB.logout();">Logout</button></p>
+</div>
+
+<script>
+  function loginUser() {    
+    FB.login(function(response) { }, {scope:'read_friendlists'});     
+    }
+</script>
 <div class="fb-login-button" data-perms="read_friendlists" data-show-faces="false" data-width="200" data-max-rows="1"></div>
 <form method="link" action="create/create_event_type.php">
 <input type="submit" value="Create an Event"></form>
@@ -26,3 +56,9 @@
 </body>
 
 </html>
+<style>
+  body.connected #login { display: none; }
+  body.connected #logout { display: block; }
+  body.not_connected #login { display: block; }
+      body.not_connected #logout { display: none; }
+</style>
