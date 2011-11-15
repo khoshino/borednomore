56fb4e59b0fc6e520c1d4b8622fe9f69d963938f
Isabelle Bounkeua <dangergirl08@gmail.com>
2011-11-14 19:59:02 -0800

    working on list buttons

--- events/searchListings.php
+++ events/searchListings.php
@@ -80,7 +80,7 @@ mysql_close($con);
 			$name  = $eventArray['name'];
 			$pgId = "event" . $eventArray['e_id'];
 			$pgTitle = $pgId . "_" . $name;
-			$startTime = $eventArray['start_time']; //TODO: convert this to human readable format
+			$startTime = $eventArray['start_time']; //TODO: convert this to human readable format 
 			$dmin = $eventArray['duration'];
 			$duration = ((int)$eventArray['duration']/60) . 'hr ' . ($dmin % 60) . 'min'; //TODO: convert this to human readable format ie. _hr_min
 			$eventButton = '<li><a href = "#'. $pgId . '"> ' . $name . '</a><br/></li>';
