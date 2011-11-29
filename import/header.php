c9b0e52b26e65357d34f55eab61a67603eb16c76
Isabelle Bounkeua <dangergirl08@gmail.com>
2011-11-28 23:57:35 -0800

    experimenting with paragraph tags and list tags

--- import/header.php
+++ import/header.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css" />
-<link rel= "stylesheet" href="./import/main.css"/>
+<link rel= "stylesheet" href="http://borednomore.cs147.org/recent/import/main.css"/>
 <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script> 
 
